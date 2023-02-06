5c14954c9dacb2aea8c3fdefff962104150434c2
juan <tecnologia@chefcompany.co>
2023-02-06 13:35:14 -0500

    prompt and no prompt v2

--- client/no-prompt.js
+++ client/no-prompt.js
@@ -155,7 +155,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://dentsu-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
